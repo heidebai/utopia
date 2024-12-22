6869a42d3fd5766514d387f57d1d80ee31ec8459
heibai <121407778+heidebai@users.noreply.github.com>
2024-12-22 17:08:32 +0800

    Update m_jd_sign.js

--- utopia/m_jd_sign.js
+++ utopia/m_jd_sign.js
@@ -1,35 +1,44 @@
 //30 8,12,22,23 * * *
-const {Env,redis} = require('./utopia');
+const {Env, redis} = require('./utopia');
 const $ = new Env('M京东签到')
 $.version = "v1.0.0"
-$.cookieTypes = $.cookieTypes || ["master", "ql", "redis", 'hot']
-$.origin="https://pro.m.jd.com"
-$.referer="https://pro.m.jd.com/mall/active/"
-$.algoUrl="https://pro.m.jd.com/mall/active/"
-$.h5stType = "test"
+$.cookieTypes = $.cookieTypes || ["master", "ql", "redis"]
+$.origin = "https://pro.m.jd.com"
+$.referer = "https://pro.m.jd.com/mall/active/"
+$.algoUrl = "https://pro.m.jd.com/mall/active/"
+$.h5stVersion = "api"
+
 class Task extends Env {
     constructor() {
         super();
     }
-    async exec () {
+
+    async exec() {
         let url = `https://api.m.jd.com/client.action`
         let {data} = await this.api(url, {
             appId: '9d49c',
             functionId: 'signBeanAct',
             appid: 'signed_wh5_ihub',
-            clientVersion: '1.0.0',
-            client: 'H5',
+            clientVersion: this.clientVersion,
+            client: this.client,
             body: {},
-            version: '4.7',
             ua: this.UA,
             t: true,
-        }, {h5st: true,proxy:true});
+            ext: {
+                screen: "390*844",
+                uuid: this.uuid,
+                networkType: "wifi",
+                eid: "",
+                d_model: "0-2-999",
+                dosVersion: ""
+            }
+        }, {h5st: true, proxy: true});
         //this.log(data)
-        if (data?.status === '2' || data?.errorCode==="S100") {
+        if (data?.status === '2' || data?.errorCode === "S100") {
             await this.runCached()
         }
-        if(data?.newUserAward){
-            this.log(data.newUserAward.awardList[data.newUserAward.continueDays-1].beanCount+"京豆")
+        if (data?.newUserAward) {
+            this.log(data.newUserAward.awardList[data.newUserAward.continueDays - 1].beanCount + "京豆")
             await this.runCached()
             return
         }
@@ -44,6 +53,5 @@ class Task extends Env {
         }
     }
 }
-
 
 $.start(Task)
